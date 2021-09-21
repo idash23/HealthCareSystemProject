5ffb80dfc92d4700eaf4a2aa69b4cb2ef39f3c5a
Jeff Maxwell <jeff@tcnsolutions.com>
2021-09-21 10:49:17 -0500

    Add dashboard and login code

--- src/main/java/edu/okcu/healthcaresystem/LoginController.java
+++ src/main/java/edu/okcu/healthcaresystem/LoginController.java
@@ -2,8 +2,9 @@ package edu.okcu.healthcaresystem;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import edu.okcu.healthcaresystem.models.LoginInfo;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class LoginController {
@@ -11,6 +12,19 @@ public class LoginController {
 	@GetMapping("/login")
 	public String login() {
 		return "login";
+	}
+
+	@PostMapping(value="/login-post")
+	public String loginPost(@RequestParam("username") String username, 
+							@RequestParam("password") String password,
+							Model model) {
+		if (username.equals("demo") && password.equals("demo")) {
+			return "dashboard";
+		} else {
+			String message = "Error logging in";
+			model.addAttribute("message", message);
+			return "login";
+		}
 	}
 
 }
