54ca386d20d7b691fb0abebb2574d4e327780ed2
Jeff Maxwell <jeff@tcnsolutions.com>
2021-09-28 15:05:08 -0500

    Reload everthing from IntelliJ template

---
+++ src/main/java/edu/okcu/healthcaresystem/HealthcaresystemApplication.java
@@ -0,0 +1,13 @@
+package edu.okcu.healthcaresystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcaresystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthcaresystemApplication.class, args);
+    }
+
+}
--- src/main/java/edu/okcu/healthcaresystem/LoginController.java
+++ src/main/java/edu/okcu/healthcaresystem/LoginController.java
@@ -2,58 +2,59 @@ package edu.okcu.healthcaresystem;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-	@GetMapping("/login")
-	public String login() {
-		return "login";
-	}
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 
-	@GetMapping("/register")
-	public String register() {
-		return "register";
-	}
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
 
-	@GetMapping("/forget-password")
-	public String forget() {
-		return "forget-password";
-	}
+    @GetMapping("/forget-password")
+    public String forget() {
+        return "forget-password";
+    }
 
-	@PostMapping(value="/login-post")
-	public String loginPost(@RequestParam("username") String username, 
-							@RequestParam("password") String password,
-							Model model) {
-		if (username.equals("demo") && password.equals("demo")) {
-			return "dashboard-doctor";
-		} else {
-			String message = "Error logging in";
-			model.addAttribute("message", message);
-			return "login";
-		}
-	}
+    @PostMapping(value="/login-post")
+    public String loginPost(@RequestParam("username") String username,
+                            @RequestParam("password") String password,
+                            Model model) {
+        if (username.equals("demo") && password.equals("demo")) {
+            return "dashboard-doctor";
+        } else {
+            String message = "Error logging in";
+            model.addAttribute("message", message);
+            return "login";
+        }
+    }
 
-	@PostMapping(value="/register-post")
-	public String registerPost(@RequestParam("password") String password, 
-							@RequestParam("cPassword") String cPassword,
-							Model model) {
-		if (password.equals(cPassword)) {
-			return "login";
-		} else {
-			String message = "Passwords are not matching";
-			model.addAttribute("message", message);
-			return "register";
-		}
-	}
+    @PostMapping(value="/register-post")
+    public String registerPost(@RequestParam("password") String password,
+                               @RequestParam("cPassword") String cPassword,
+                               Model model) {
+        if (password.equals(cPassword)) {
+            return "login";
+        } else {
+            String message = "Passwords are not matching";
+            model.addAttribute("message", message);
+            return "register";
+        }
+    }
 
-	@PostMapping(value="/forget-post")
-	public String forgetPost(@RequestParam("email") String email) {
-		//check if email exist
-		
-		return "forget-password";
-	}
+    @PostMapping(value="/forget-post")
+    public String forgetPost(@RequestParam("email") String email) {
+        //check if email exist
+
+        return "forget-password";
+    }
 
 }
